eb4fd5b103bee68631a58bc9de774b217cc62abb
Piskotka <timonfillo10@gmail.com>
2025-03-12 22:18:39 +0100

    init

---
+++ src/main/java/sk/piskotka/voidrunner/Voidrunner.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sk.piskotka.voidrunner;
+
+/**
+ *
+ * @author piskotka
+ */
+public class Voidrunner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
